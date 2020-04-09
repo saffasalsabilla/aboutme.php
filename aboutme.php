f5b2919cf364cfafbbb1004e63384bd85ca224ba
Saffa Salsabilla <55771189+saffasalsabilla@users.noreply.github.com>
2020-04-09 10:22:04 +0700

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,8 @@
+<?php
+echo 'Nama : Saffa Salsabilla <br>' ;
+echo 'Kelas : XI RPL 1 <br>';
+echo 'Asal Sekolah : SMK Taruna Bhakti Depok <br>';
+echo 'Alamat : Jln. Raya Bogor KM 35 Simpangan Depok No. 14 <br>';
+
+
+?>
